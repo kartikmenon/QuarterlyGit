16d72b425e3f6173611c9fe729ca43e65d4d9d6a
kartikmenon <kartikeya.menon@gmail.com>
2014-06-07 00:02:58 -0400

    Possible multilingual

---
+++ scripts/randomizer_ling.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+/* Author Kartik Menon 
+	The Quarterly of East Asian Studies
+	JavaScript
+*/
+function swap() {
+		var images = new Array("../img/banner1.jpg", "../img/banner2.jpg", "../img/banner3.jpg", "../img/banner4.jpg");
+		var n = Math.floor(Math.random() * images.length);
+		
+		document.write('<img src = "' +images[n]+ '">');
+}
